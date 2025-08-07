2fd5ae5a8e51e08a8a2f9fcdbf9918052f18c63c
Orion Gestor <orion@gestor.com>
2025-08-06 21:34:55 -0300

    🔧 CORREÇÃO SINTAXE - Corrigir erro de sintaxe no Inventory.js - Remover comentário JSX mal posicionado - Resolver erro de build no Netlify - Garantir compilação correta do React

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -358,7 +358,6 @@ const Inventory = () => {
         </div>
       </div>
 
-      {/* Modal de Movimento de Estoque */}
       {showMovementModal && selectedProduct && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
           <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
